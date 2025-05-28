883ee735d93fb260457744ff2015c8b2342e295c
albab83 <141700905+albab83@users.noreply.github.com>
2025-05-28 07:39:52 +0700

    update santri dengan web socket

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,12 @@
 const { User } = require("../models");
+const { get } = require("../routes/authRoutes");
 
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.findAll({
-      attributes: { exclude: ["password"] }
+      attributes: { exclude: ["password"] },
     });
+    req.app.get("io").emit("updateSantri");
     res.json(users);
   } catch (error) {
     res.status(500).json({ message: "Gagal mengambil data users", error });
